7d8f76c3b030b71acf6591256972369f9fe57369
MrZhangshengbaba <95+MrZhangshengbaba@users.noreply.github.com>
2018-03-08 18:02:46 +0800

    .

--- AC/src/main/java/com/ac/MyProcess/receiveTask/TestReceiveTask.java
+++ AC/src/main/java/com/ac/MyProcess/receiveTask/TestReceiveTask.java
@@ -70,7 +70,7 @@ public class TestReceiveTask {
 		Execution execution = defaultProcessEngine.getRuntimeService()
 		.createExecutionQuery()
 		.processInstanceId(processinstance.getId())
-		.activityId("activityId")
+		.activityId("servicetask1")
 		.singleResult();
 		/*使用流程变量设置当日销售额，用来传递业务参数*/
 		defaultProcessEngine.getRuntimeService().setVariable(execution.getId(), "汇总当日销售额",21000);
@@ -78,50 +78,16 @@ public class TestReceiveTask {
 		defaultProcessEngine.getRuntimeService().signal(execution.getId());
 		
 		
-	}
-
-	/**
-	 * 查询当前人的个人任务
-	 * 
-	 */
-
-	@Test
-	public void findMyPersontask() {
-		String assignee = "赵晓柳";// 默认财务 赵晓柳 申请人张爸爸
-		List<Task> list = defaultProcessEngine.getTaskService()// 与正在执行的任务相关的 表act_ru_task
-				.createTaskQuery() // 创建任务查询对象
-				.taskAssignee(assignee) // 指定个人任务查询，指定班里人
-				.list();
-		if (list != null && list.size() > 0) {
-			for (Task task : list) {
-				System.out.println("任务id:" + task.getId());
-				System.out.println("任务名称:" + task.getName());
-				System.out.println("任务的创建时间:" + task.getCreateTime());
-				System.out.println("任务办理人:" + task.getAssignee());
-				System.out.println("流程实例id:" + task.getProcessInstanceId());
-				System.out.println("执行对象id:" + task.getExecutionId());
-				System.out.println("流程定义id:" + task.getProcessDefinitionId());
-				System.out.println("##########################################################");
-			}
-		}
-	}
-
-	/**
-	 * 完成我的任务
-	 * 
-	 */
-
-	@Test
-	public void completeMyTask() {
-		String taskId = "3604";
-		// 完成任务的同时，使用流程变量，使用流程变量来指定完成任务后，下一个连线
-
-		Map<String, Object> variables = new HashMap<>();
-		variables.put("money", 500);
-		defaultProcessEngine.getTaskService().complete(taskId, variables);
-		;
-		System.out.println("完成任务的ID：" + taskId);
-		;
-
+		Execution execution1 = defaultProcessEngine.getRuntimeService()
+				.createExecutionQuery()
+				.processInstanceId(processinstance.getId())
+				.activityId("servicetask1")
+				.singleResult();
+		/*使用流程变量设置当日销售额，用来传递业务参数*/
+		defaultProcessEngine.getRuntimeService().setVariable(execution1.getId(), "给老板发送短信","已汇总");
+		/*向后执行一步*/
+		defaultProcessEngine.getRuntimeService().signal(execution1.getId());
+				
+		
 	}
 }
